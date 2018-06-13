ed837ee8d4074c08559f27330798a7436f4a1dca
Zu <zuoficial@gmail.com>
2018-06-13 04:28:30 -0500

    Termino revision Posibles implementanciones - Agregar filtro para referencia en posiciones - Arreglar el scrollview en pantalla de ingreso de datos para posicion

--- app/src/main/java/com/smoowy/xTrade/MainActivity.java
+++ app/src/main/java/com/smoowy/xTrade/MainActivity.java
@@ -803,11 +803,11 @@ public class MainActivity extends AppCompatActivity
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         } else {
-          /*  Intent i = getBaseContext().getPackageManager().
+            Intent i = getBaseContext().getPackageManager().
                     getLaunchIntentForPackage(getBaseContext().getPackageName());
             i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
             i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            startActivity(i);*/
+            startActivity(i);
             super.onBackPressed();
         }
     }
